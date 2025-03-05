8c9e78965835bc685ff3c0afed5c3f60e5167d4d
jfernan2 <Javier.Fernandez@cern.ch>
2025-03-05 12:30:25 +0100

    Create samples.py

---
+++ test/samples.py
@@ -0,0 +1,32 @@
+samples = {
+    # MUON GUN
+    "mu_PU0"   : "/Mu_FlatPt2to100-pythia8-gun/PhaseIITDRSpring19DR-NoPU_106X_upgrade2023_realistic_v3-v1/GEN-SIM-DIGI-RAW",
+    "mu_PU140" : "/SingleMuon_Pt-2To50_Eta-0p9To2p85-gun/Phase2Spring24DIGIRECOMiniAOD-PU140_Trk1GeV_140X_mcRun4_realistic_v4-v1/GEN-SIM-DIGI-RAW-MINIAOD",
+    "mu_PU200" : "/Mu_FlatPt2to100-pythia8-gun/PhaseIITDRSpring19DR-PU200_106X_upgrade2023_realistic_v3-v2/GEN-SIM-DIGI-RAW",
+
+    #JPsiToMuMu
+    "jpsimumu" : "/JPsiToMuMu_Pt-0To100-pythia8-gun/Phase2Spring24DIGIRECOMiniAOD-PU200_Trk1GeV_140X_mcRun4_realistic_v4-v2/GEN-SIM-DIGI-RAW-MINIAOD",
+
+    #MinBias
+    "minbias_PU140" : "/MinBias_TuneCP5_14TeV-pythia8/Phase2Spring24DIGIRECOMiniAOD-PU140_Trk1GeV_140X_mcRun4_realistic_v4-v1/GEN-SIM-DIGI-RAW-MINIAOD",
+    "minbias_PU200" : "/MinBias_TuneCP5_14TeV-pythia8/Phase2Fall22DRMiniAOD-PU200_125X_mcRun4_realistic_v2-v1/GEN-SIM-DIGI-RAW-MINIAOD",
+    "mb_PU200" : "/MinBias_TuneCP5_14TeV-pythia8/Phase2Spring24DIGIRECOMiniAOD-PU200ALCA_140X_mcRun4_realistic_v4-v2/GEN-SIM-DIGI-RAW-MINIAOD",
+
+    #NEUTRINO GUN
+    "nu_PU250" : "/Nu_E10-pythia8-gun/PhaseIITDRSpring19DR-PU250_106X_upgrade2023_realistic_v3_ext2-v1/GEN-SIM-DIGI-RAW",
+    "nu_PU200" : "/Nu_E10-pythia8-gun/PhaseIITDRSpring19DR-PU200_106X_upgrade2023_realistic_v3-v3/GEN-SIM-DIGI-RAW",
+
+    #DISPLACED MUONS
+    "DM_pt10-30_PU200" : "/DisplacedMuons_Pt10to30_Dxy0to3000-pythia8-gun/PhaseIITDRSpring19DR-PU200_106X_upgrade2023_realistic_v3-v1/GEN-SIM-DIGI-RAW",
+    "DM_pt10-30_PU0"   : "/DisplacedMuons_Pt10to30_Dxy0to3000-pythia8-gun/PhaseIITDRSpring19DR-NoPU_106X_upgrade2023_realistic_v3-v1/GEN-SIM-DIGI-RAW",
+    "DM_pt30-100_PU0"  : "/DisplacedMuons_Pt30to100_Dxy0to3000-pythia8-gun/PhaseIITDRSpring19DR-NoPU_106X_upgrade2023_realistic_v3-v1/GEN-SIM-DIGI-RAW",
+
+    #ALBERTO'S SAMPLES
+    "DM_Alberto_20"    : "/HTo2LongLivedTo4mu_MH-200_MFF-50_CTau-20mm_TuneCP5_13TeV_pythia8/escalant-MC2018_Benchmark_2mu2jets_June2019-GSR-testForL1-v2-5aa1749307f00d6302ec929df355f761/USER",
+    "DM_Alberto_200"   : "/HTo2LongLivedTo4mu_MH-200_MFF-50_CTau-200mm_TuneCP5_13TeV_pythia8/escalant-MC2018_Benchmark_2mu2jets_June2019-GSR-testForL1-v2-5aa1749307f00d6302ec929df355f761/USER",
+    "DM_Alberto_2000"  : "/HTo2LongLivedTo4mu_MH-200_MFF-50_CTau-2000mm_TuneCP5_13TeV_pythia8/escalant-MC2018_Benchmark_2mu2jets_June2019-GSR-testForL1-v2-5aa1749307f00d6302ec929df355f761/USER",
+
+    #DYLL M50 PU200
+    "DYLL_M50_PU200" : "/DYToLL_M-50_TuneCP5_14TeV-pythia8/Phase2Spring24DIGIRECOMiniAOD-PU200_Trk1GeV_140X_mcRun4_realistic_v4-v1/GEN-SIM-DIGI-RAW-MINIAOD",
+
+    }
